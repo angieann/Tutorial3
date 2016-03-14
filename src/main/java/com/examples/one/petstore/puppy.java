bcd009180529e095c794858f3ab6cead098f9867
Elise Callus <elise.gabrielle@hotmail.com>
2016-03-14 08:58:12 +0100

    Puppy Added

---
+++ src/main/java/com/examples/one/petstore/puppy.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.examples.one.petstore;
+
+/**
+ *
+ * @author userElise
+ */
+public class puppy {
+    
+    int id;
+    String name;
+    String[] bread ={"Labrador","Doberman", "Pitbull"} ;
+    boolean loudBark;
+    int exerciseRequiredPerDay;
+    
+}
