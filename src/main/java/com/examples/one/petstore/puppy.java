f539841d917119c89d7f86f2cc62bed47a3a0524
Elise Callus <elise.gabrielle@hotmail.com>
2016-03-14 09:09:37 +0100

    Final Puppy

--- src/main/java/com/examples/one/petstore/puppy.java
+++ src/main/java/com/examples/one/petstore/puppy.java
@@ -5,6 +5,8 @@
  */
 package com.examples.one.petstore;
 
+import java.util.Arrays;
+
 /**
  *
  * @author userElise
@@ -13,8 +15,36 @@ public class puppy {
     
     int id;
     String name;
-    String[] bread ={"Labrador","Doberman", "Pitbull"} ;
+    String[] breed ={"Labrador","Doberman", "Pitbull"} ;
     boolean loudBark;
-    int exerciseRequiredPerDay;
+    int[] exerciseRequiredPerDay= {0,1,2,3,4};
+    boolean friendlyWithChildren;
+    
+    public puppy(int id, String name, String breed){
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setBreed(String[] breed){
+        this.breed = Arrays.copyOf(breed, breed.length);
+    }
+    
+    public String[] getBreed(){
+        return Arrays.copyOf(breed, breed.length);
+    }
     
 }
